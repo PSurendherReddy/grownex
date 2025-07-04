d60dd39c5e06235f58b7146c6e37702fdcb60f3e
Arjun Gudipalli <arjun312001@gmail.com>
2025-07-04 15:54:49 +0000

    add arrowmark animation to buttons when hover

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -40,14 +40,20 @@ export interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, variant, size, asChild = false, children, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
+    const showArrow = variant !== 'link' && variant !== 'ghost' && size !== 'icon';
+
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={cn(buttonVariants({ variant, size, className }), {
+          'btn-arrow-hover': showArrow,
+        })}
         ref={ref}
         {...props}
-      />
+      >
+        {showArrow ? <span className="btn-text-wrapper">{children}</span> : children}
+      </Comp>
     )
   }
 )
